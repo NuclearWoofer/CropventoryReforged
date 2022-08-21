768e931af6f51beb5b81168dc574340ae31392b8
Michael Lopez <ml102753@harriscomputer.com>
2022-08-21 09:40:47 -0400

    god i wish i kept up with this

--- model/db.php
+++ model/db.php
@@ -8,19 +8,18 @@
 //**************************
 // PGADMIN DATABASE CRED
 //**************************
-$ini = parse_ini_file( __DIR__ . '/dbconfig.ini');
+    // $ini = parse_ini_file( __DIR__ . '/dbconfig.ini');
+    // $g_set_sql_mode = true;
 
-$g_set_sql_mode = true;
-
-$db = new PDO(  "pgsql:host=" . $ini['servername'] . 
-                ";port=" . $ini['port'] . 
-                ";dbname=" . $ini['dbname'], 
-                $ini['username'], 
-                $ini['password']);
+    // $db = new PDO(  "pgsql:host=" . $ini['servername'] . 
+    //                 ";port=" . $ini['port'] . 
+    //                 ";dbname=" . $ini['dbname'], 
+    //                 $ini['username'], 
+    //                 $ini['password']);
 
 
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // THESE CREDS GO INTO dbconfig.ini TO CONNECT TO PGADMIN
 // servername = ec2-50-19-255-190.compute-1.amazonaws.com
@@ -28,3 +27,13 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // username = pmuibcycqdcjyx
 // password = cd83ea5e98c4341ba6c296aba89a4a0b95725b709ef4705ee37130366b093c97
 // dbname = dbbrve4ld95mpp
+
+
+
+//**************************
+// PGADMIN DATABASE CRED *RIPPED FROM PHP|PGSQL MANUAL
+//**************************
+
+    $db_connection = pg_connect("host=ec2-50-19-255-190.compute-1.amazonaws.com dbname=dbbrve4ld95mpp user=pmuibcycqdcjyx password=cd83ea5e98c4341ba6c296aba89a4a0b95725b709ef4705ee37130366b093c97");
+?>
+
